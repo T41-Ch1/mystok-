9e88bdf5ac6fa50df607cafdb88e196dc7df9cfd
T41-Ch1 <dekabitasp@gmail.com>
2020-11-29 17:36:08 +0900

    [mod]db_url-package-modify.sh - correct path to *.java

--- .github/scripts/db_url-package-modify.sh
+++ .github/scripts/db_url-package-modify.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i _backup -e s@localhost:3306@127.0.0.1@g -e s@pac1@mystok@g *.java
+sed -i _backup -e s@localhost:3306@127.0.0.1@g -e s@pac1@mystok@g src/main/java/*.java
 rm -f *.java_backup
