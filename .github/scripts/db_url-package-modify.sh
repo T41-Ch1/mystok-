3228acbc0868beffcdfd2a386225b062191db5f0
T41-Ch1 <dekabitasp@gmail.com>
2020-12-18 14:55:16 +0900

    update: to change *.jsp's packageName

--- .github/scripts/db_url-package-modify.sh
+++ .github/scripts/db_url-package-modify.sh
@@ -3,3 +3,5 @@ sed -i _backup -e s@localhost:3306/j2a1b@127.0.0.1:3306/mystok@g -e s@pac1@mysto
 rm -f src/main/java/*.java_backup
 sed -i _backup -e s@pac1@mystok@g src/main/java/*/*.java
 rm -f src/main/java/*/*.jaca_backup
+sed -i _backup -e s@pac1@mystok@g src/main/webapp/*.jsp
+rm -f src/main/webapp/*.jsp_backup
