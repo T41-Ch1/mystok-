73644b868267859bdc2c4899bd9914edf2f13b8d
T41-Ch1 <dekabitasp@gmail.com>
2021-01-08 13:52:28 +0900

    change: Util.java DB_URL

--- src/main/java/func/Util.java
+++ src/main/java/func/Util.java
@@ -38,7 +38,7 @@ public class Util {
 
 		try (
 				Connection conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/j2a1b?serverTimezone=JST","root","password");
+					"jdbc:mysql://127.0.0.1:3306/mystok?serverTimezone=JST","root","password");
 				PreparedStatement prestmt = conn.prepareStatement(sql)) {
 			prestmt.setString(1,userName);
 			prestmt.setInt(2,recipeID);
@@ -81,7 +81,7 @@ public class Util {
 
 		try (
 				Connection conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/j2a1b?serverTimezone=JST","root","password");
+					"jdbc:mysql://127.0.0.1:3306/mystok?serverTimezone=JST","root","password");
 				PreparedStatement prestmt = conn.prepareStatement(sql)) {
 			prestmt.setString(1, userName);
 			for (int i = 0; i < recipeID.size(); i++) {
@@ -119,7 +119,7 @@ public class Util {
 
 		try (
 				Connection conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/j2a1b?serverTimezone=JST","root","password");
+					"jdbc:mysql://127.0.0.1:3306/mystok?serverTimezone=JST","root","password");
 				PreparedStatement prestmt = conn.prepareStatement(sql)) {
 			prestmt.setString(1, userName);
 			try (ResultSet rs = prestmt.executeQuery()) {
